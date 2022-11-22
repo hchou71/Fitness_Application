bdc24dab58b2a72798ffcae2df8d438ed08df244
jckim1116 <jck1116@uw.edu>
2022-11-21 17:04:49 -0800

    make changes to app

--- src/App.js
+++ src/App.js
@@ -1,55 +1,72 @@
 import React from 'react';
 import { Header, Features, Footer } from './Home';
 import { NewExercise } from './new-exercise.js';
+import { DisccusionPage } from './discussion.js';
 
 function App() {
   return (
+    <div>
+    <Header/>
+
+    <Features/>
+    {/* <NewExercise/> */}
+
+    <DisccusionPage/>
+
+    <Footer/>
+
+    </div>
+    )
+}
+
+export default App;
+
+
+ {/*
     <body>
-      <nav class="navbar navbar-expand-lg navbar-dark" >
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Fitness 340</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+      <nav className="navbar navbar-expand-lg navbar-dark" >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Fitness 340</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
             aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-label="Home Page" href="index.html">Home</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-label="Home Page" href="index.html">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-label="Progress Page" href="progress.html">Progress Tracker</a>
+              <li className="nav-item">
+                <a className="nav-link" aria-label="Progress Page" href="progress.html">Progress Tracker</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-label="Exercise Search Page" href="exercise.html">Exercise Searcher</a>
+              <li className="nav-item">
+                <a className="nav-link" aria-label="Exercise Search Page" href="exercise.html">Exercise Searcher</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-label="Discussion page" href="discussion.html">Discussion</a>
+              <li className="nav-item">
+                <a className="nav-link" aria-label="Discussion page" href="discussion.html">Discussion</a>
               </li>
             </ul>
           </div>
         </div>
       </nav >
 
-      <header class="home-header">
+      <header className="home-header">
         <Header />
       </header>
 
-      <main class="home-main mt-5">
-        <div class="container container-progress">
+      <main className="home-main mt-5">
+        <div className="container container-progress">
           <Features />
         </div>
       </main>
 
-      <footer class="text-center text-lg-start bg-white text-muted">
+      <footer className="text-center text-lg-start bg-white text-muted">
         <Footer />
-        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        <div className="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
           &copy; 2022 FITNESS 340:
-          <a class="text-reset fw-bold" href="#">fitness340.com</a>
+          <a className="text-reset fw-bold" href="#">fitness340.com</a>
         </div>
       </footer>
-    </body>)
-}
-
-export default App;
+    </body>
+  */}
--- src/Home.js
+++ src/Home.js
@@ -2,37 +2,37 @@ import React from 'react';
 
 export function Header(props) {
     return (
-        <div class="container-fluid">
+        <div className="container-fluid">
             <h1>FITNESS 340</h1>
-            <p class="lead">Our goal is to provide you with the right set of tools to accomplish any fitness goal you set for yourself!</p>
-            <p><a class="btn btn-lg" href="#" role="button">Learn more »</a></p>
+            <p className="lead">Our goal is to provide you with the right set of tools to accomplish any fitness goal you set for yourself!</p>
+            <p><a className="btn btn-lg" href="#" role="button">Learn more »</a></p>
         </div>
     )
 }
 
 export function Features(props) {
     return (
-        < div class="row" >
-            < div class=" col-lg-4 col-12 p-2 border rounded" >
-                <h2 class="text-dark">Progress Tracker</h2>
+        < div className="row" >
+            < div className=" col-lg-4 col-12 p-2 border rounded" >
+                <h2 className="text-dark">Progress Tracker</h2>
                 <p>Track your fitness progress here with our easy to use Fitness Tracker.</p>
-                <a class="btn" href="progress.html" role="button">Track »</a>
+                <a className="btn" href="progress.html" role="button">Track »</a>
             </div >
 
-            <div class="col-lg-4 col-12 p-2 border rounded">
-                <h2 class="text-dark">Search Your Exercise</h2>
+            <div className="col-lg-4 col-12 p-2 border rounded">
+                <h2 className="text-dark">Search Your Exercise</h2>
                 <p>Not sure which muscle group to target today? Use our Exercise Search feature to find the
                     perfect workout for you! </p>
-                <a class="btn" href="exercise.html" role="button">Find Exercises »</a>
+                <a className="btn" href="exercise.html" role="button">Find Exercises »</a>
             </div>
 
-            <div class=" col-lg-4 col-12 p-2 border rounded">
-                <h2 class="text-dark">Discussion & Chat</h2>
-                <p>Have questions about <fitnes></fitnes>s and living a healthy lifestyle? Post a question and
+            <div className=" col-lg-4 col-12 p-2 border rounded">
+                <h2 className="text-dark">Discussion & Chat</h2>
+                <p>Have questions about fitness and living a healthy lifestyle? Post a question and
                     discuss
                     with fellow Fitness 340 members today!
                 </p>
-                <p><a class="btn" href="discussion.html" role="button">Discuss »</a></p>
+                <p><a className="btn" href="discussion.html" role="button">Discuss »</a></p>
             </div>
         </div >
     )
@@ -40,31 +40,31 @@ export function Features(props) {
 
 export function Footer(props) {
     return (
-        <div class="text-center text-md-start mt-5">
-            <div class="row mt-3">
-                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                    <h6 class="text-uppercase fw-bold mb-3">
+        <div className="text-center text-md-start mt-5">
+            <div className="row mt-3">
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h3 className="text-uppercase fw-bold mb-3">
                         Fitness 340
-                    </h6>
+                    </h3>
                     <p>
                         Our goal is to foster the habits of work out exercises. We look forward to help on people
                         for all level.
                     </p>
                 </div>
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    <h6 class="text-uppercase fw-bold mb-3">Contact</h6>
-                    <ul class=" list-unstyled ">
-                        <li class=" pb-1">
-                            <a class="text-muted text-decoration-none" href="mailto:echoi3@uw.edu">echoi3@uw.edu</a>
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                    <h6 className="text-uppercase fw-bold mb-3">Contact</h6>
+                    <ul className=" list-unstyled ">
+                        <li className=" pb-1">
+                            <a className="text-muted text-decoration-none" href="mailto:echoi3@uw.edu">echoi3@uw.edu</a>
                         </li>
-                        <li class=" pb-1">
-                            <a class="text-muted text-decoration-none" href="mailto:hchou71@uw.edu">hchou71@uw.edu</a>
+                        <li className=" pb-1">
+                            <a className="text-muted text-decoration-none" href="mailto:hchou71@uw.edu">hchou71@uw.edu</a>
                         </li>
-                        <li class=" pb-1">
-                            <a class="text-muted text-decoration-none" href="mailto:jck1116@uw.edu">jck1116@uw.edu</a>
+                        <li className=" pb-1">
+                            <a className="text-muted text-decoration-none" href="mailto:jck1116@uw.edu">jck1116@uw.edu</a>
                         </li>
-                        <li class=" pb-1">
-                            <a class="text-muted text-decoration-none" href="mailto:vinliu20@uw.edu">vinliu20@uw.edu</a>
+                        <li className=" pb-1">
+                            <a className="text-muted text-decoration-none" href="mailto:vinliu20@uw.edu">vinliu20@uw.edu</a>
                         </li>
                     </ul>
                 </div>
