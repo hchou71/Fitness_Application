c48c0137e8032a27cbca9cc851756c7a4b81e59c
hchou71 <hchou71@uw.edu>
2022-11-21 18:41:27 -0800

    prvWorkout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Navbar, Header, Features, Footer } from './Home';
+import { Navbar, HomePage, Footer } from './Home';
 import { NewExercise } from './new-exercise.js';
 import { DiscussionPage } from './discussion.js';
 import { ExercisePage } from './exercise';
 import { DetailsPage } from './exercise-details';
+import { PrvWorkout } from './PrvWorkout';
 
 function App(props) {
   return (
     <div>
       <Navbar />
-      {/* <Header /> */}
-      {/* <Features /> */}
+      {/* <HomePage /> */}
 
-      <NewExercise />
 
+      {/* <NewExercise /> */}
+      <PrvWorkout />
 
       <Footer />
 
@@ -24,51 +25,3 @@ function App(props) {
 export default App;
 
 
-{/*
-    <body>
-      <nav className="navbar navbar-expand-lg navbar-dark" >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Fitness 340</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-label="Home Page" href="index.html">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-label="Progress Page" href="progress.html">Progress Tracker</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-label="Exercise Search Page" href="exercise.html">Exercise Searcher</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" aria-label="Discussion page" href="discussion.html">Discussion</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav >
-
-      <header className="home-header">
-        <Header />
-      </header>
-
-      <main className="home-main mt-5">
-        <div className="container container-progress">
-          <Features />
-        </div>
-      </main>
-
-      <footer className="text-center text-lg-start bg-white text-muted">
-        <Footer />
-        <div className="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-          &copy; 2022 FITNESS 340:
-          <a className="text-reset fw-bold" href="#">fitness340.com</a>
-        </div>
-      </footer>
-    </body>
-*/}
--- src/Home.js
+++ src/Home.js
@@ -12,10 +12,10 @@ export function Navbar(props) {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-label="Home Page" href="index.html">Home</a>
+                        <li className="nav-item ">
+                            <a className="nav-link" aria-label="Home Page" href="index.html">Home</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ">
                             <a className="nav-link" aria-label="Progress Page" href="progress.html">Progress Tracker</a>
                         </li>
                         <li className="nav-item">
@@ -31,7 +31,25 @@ export function Navbar(props) {
     )
 }
 
-export function Header(props) {
+export function HomePage(props) {
+    return (
+        <div>
+            <header className="home-header">
+                <Header />
+            </header >
+
+            <main className="home-main mt-5">
+                <div className="container container-progress">
+                    <Features />
+                </div>
+            </main >
+        </div>
+
+
+    )
+}
+
+function Header(props) {
     return (
         <div className="container-fluid">
             <h1>FITNESS 340</h1>
