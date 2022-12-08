a934e325caa480752d4056ddc962c0b0a77d5411
hchou71 <hchou71@uw.edu>
2022-12-08 14:10:25 -0800

    ham menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Navbar } from './Navbar';
+import { NavBar } from './Navbar';
 import { Footer } from './Footer';
 import { HomePage } from './Home';
 import { NewExercise } from './new-exercise.js';
@@ -10,18 +10,19 @@ import { DetailsPage } from './exercise-details';
 import { PastExercises } from './past-exercises';
 import { ProgressPage } from './progress-page.js';
 import { LoginPage } from './login';
+import { Example } from './Navbar';
 
 function App(props) {
   return (
-    <div> 
+    <div>
       {/* classname="home-exa"  */}
-      <Navbar />
+      <NavBar />
 
       <Routes>
         <Route path="login" element={<LoginPage />} />
         <Route path="" element={<HomePage />} />
         <Route path="exercise-details" element={<DetailsPage exerciseObj={props.exercises[0]} />} />
-        <Route path="track-progress" element={<ProgressPage />} /> 
+        <Route path="track-progress" element={<ProgressPage />} />
         <Route path="previous-workouts" element={<PastExercises />} />
         <Route path="new-exercise" element={<NewExercise />} />
         <Route path="search" element={<ExercisePage exercises={props.exercises} />} />
