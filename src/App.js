fc67c92ad08ab4b7d7a3daefd92608b4112facac
Elliot Choi <echoi3@uw.edu>
2022-12-09 17:58:40 -0800

    attempted adding protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import { NavBar } from './Navbar';
 import { Footer } from './Footer';
 import { HomePage } from './Home';
@@ -11,22 +11,48 @@ import { PastExercises } from './past-exercises';
 import { ProgressPage } from './progress-page.js';
 import { LoginPage } from './login';
 import { Example } from './Navbar';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function App(props) {
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (firebaseUser) => {
+      if(firebaseUser) {
+        firebaseUser.userId = firebaseUser.uid;
+        firebaseUser.userName = firebaseUser.displayName;
+        setCurrentUser(firebaseUser);
+      } else {
+        console.log("Logged Out");
+      }
+    })
+  })
+
+  function RequireAuth() {
+    if (currentUser === null) {
+      return <Navigate to="/login" />;
+    } else {
+      return <Outlet />;
+    }
+  }
+  
   return (
     <div>
       {/* classname="home-exa"  */}
       <NavBar />
-
       <Routes>
+        <Route element={<RequireAuth />} >
+          <Route path="track-progress" element={<ProgressPage />} />
+          <Route path="previous-workouts" element={<PastExercises />} />
+          <Route path="new-exercise" element={<NewExercise />} />
+          <Route path="discussion" element={<PastWorkoutTest url={props.url} />} />
+        </Route>
         <Route path="login" element={<LoginPage />} />
         <Route path="" element={<HomePage />} />
         <Route path="exercise-details" element={<DetailsPage exerciseObj={props.exercises[0]} />} />
-        <Route path="track-progress" element={<ProgressPage />} />
-        <Route path="previous-workouts" element={<PastExercises />} />
-        <Route path="new-exercise" element={<NewExercise />} />
         <Route path="search" element={<ExercisePage exercises={props.exercises} />} />
-        <Route path="discussion" element={<PastWorkoutTest url={props.url} />} />
       </Routes>
 
       <Footer />
